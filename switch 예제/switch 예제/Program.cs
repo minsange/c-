a7382ec1662524ef42df6e6384dc0ccd4c8d36d2
minsange <44996616+minsange@users.noreply.github.com>
2019-10-03 17:37:33 +0900

    Add files via upload

---
+++ switch 예제/switch 예제/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace c샵예제
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
